e0a625847c5a92f5012959ac86447cec70a22aae
Salvador <salvadorpm@gmail.com>
2023-02-22 10:10:23 -0600

    Primer commit Proyecto, modulo y componente clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClientesModule } from './clientes/clientes.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClientesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
